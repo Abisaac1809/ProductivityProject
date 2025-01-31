afd45be7871d25c15ae1365b67182c65bdbdf012
Abisaac1809 <abisaaccarmona@gmail.com>
2025-01-30 22:47:34 -0400

    The main folder is added
    Each of the modules is added with its respective files

---
+++ ProductivityProject/src/Habits/HabitsMain.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Habits;
+
+/**
+ *
+ * @author Admin
+ */
+public class HabitsMain {
+    
+}
