31e833ed9b79786304ae2ec6e3a766c656c72c47
Jose <joseamendezborges@gmail.com>
2025-02-09 12:38:32 -0400

    Added default function and options in Finance module

--- ProductivityProject/src/Finances/FinanceDevelopment.java
+++ ProductivityProject/src/Finances/FinanceDevelopment.java
@@ -1,14 +1,45 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package Finances;
+import java.io.IOException;
 
-/**
- *
- * @author Admin
- */
 public class FinanceDevelopment {
+    static void fundefault(String file1, String file2) throws IOException{
+        boolean flag1 = FinanceValidation.itExist(file1);
+        boolean flag2 = FinanceValidation.itExist(file2);
+        
+        if(!flag1){
+            FinanceValidation.fileCreate(file1);
+        }
+        if(!flag2){
+            FinanceValidation.fileCreate(file2);
+        }
+    }
     
+    static int menufinance(String file1) throws IOException{
+        double num =FinanceValidation.fileHeadReading(file1);
+        System.out.println(num);
+        int option = 0;
+        System.out.println("1. Añadir saldo");
+        System.out.println("2. Restar saldo");
+        option = FinanceValidation.options();
+        return option;
+    }
+    static void addAmount(String file1) throws IOException{
+        int add =0;
+        String text = "Ingrese el saldo a agregar";
+        int num =FinanceValidation.fileHeadReading(file1);
+        System.out.println("Saldo disponible: "+num);
+        add=FinanceValidation.valEntry(text);
+        num+=add;
+        FinanceValidation.fileWriter1(file1, add);
+    }
+    
+    static void subtractAmount(String file1) throws IOException{
+        int add =0;
+        String text = "Ingrese cuanto saldo quitar";
+        int num =FinanceValidation.fileHeadReading(file1);
+        System.out.println("Saldo disponible: "+num);
+        add=FinanceValidation.valEntry(add,text);
+        num-=add;
+        FinanceValidation.fileWriter1(file1, add);
+    }
 }
