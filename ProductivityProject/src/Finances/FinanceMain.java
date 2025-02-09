0ff158001078e289cd4ec444ded59fee67faece4
Jose <joseamendezborges@gmail.com>
2025-02-09 12:14:23 -0400

    Added menu in Finance module

--- ProductivityProject/src/Finances/FinanceMain.java
+++ ProductivityProject/src/Finances/FinanceMain.java
@@ -1,14 +1,21 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package Finances;
+import java.io.IOException;
 
-/**
- *
- * @author Admin
- */
 public class FinanceMain {
-    
+    public static void main(String username) throws IOException{
+        int option=0;
+        String file1 = username+"finances1.txt";
+        String file2 = username+"finances2.txt";
+        System.out.println(file1);
+        while(option>=0 && option<=2){
+            option=FinanceDevelopment.menufinance(file1);
+            switch (option){
+                case 1: 
+                    FinanceDevelopment.addAmount(file1);
+                    break;
+                case 2: 
+                    FinanceDevelopment.subtractAmount(file1);
+            }
+        }
+    } 
 }
