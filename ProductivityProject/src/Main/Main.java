b29b9d07b8ecef9ae780192d967bef8df5d7e20a
Gabriel Vieira <nox030705@gmail.com>
2025-02-11 08:29:34 -0400

    missing input object declared in main

--- ProductivityProject/src/Main/Main.java
+++ ProductivityProject/src/Main/Main.java
@@ -4,21 +4,24 @@ import Users.UsersMain;
 import Finances.FinanceMain;
 import Habits.HabitsMain;
 import java.io.IOException;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         System.out.println("---PRODUCTIVITY PROJECT---\n");
-        String username = UsersMain.main();
+        Scanner input = new Scanner(System.in);
+        String username = UsersMain.main(input);
         int option = MainDevelopment.mainMenu(username);
         while (option != 5) {
             if (option == 1) FinanceMain.main(username);
             if (option == 2) HabitsMain.HabitsMain(username);
             if (option == 4) {
                 System.out.println("");
-                username = UsersMain.main();
+                username = UsersMain.main(input);
             }
             option = MainDevelopment.mainMenu(username);
         }
         System.out.println("\n---PROGRAMA CERRADO---");
+        input.close();
     }
 }
