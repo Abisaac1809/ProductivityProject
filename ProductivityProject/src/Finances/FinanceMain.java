7fbb07697ab68224b850f7223bd08cd83b6241ee
Jose <joseamendezborges@gmail.com>
2025-02-12 19:08:39 -0400

    Bugfix

--- ProductivityProject/src/Finances/FinanceMain.java
+++ ProductivityProject/src/Finances/FinanceMain.java
@@ -3,18 +3,18 @@ import java.io.IOException;
 
 public class FinanceMain {
     public static void main(String username) throws IOException{
-        int option=0;
+        double option=0;
         String file1 = username+"finances1.txt";
         String file2 = username+"finances2.txt";
-        System.out.println(file1);
+        FinanceDevelopment.fundefault(file1, file2);
+        System.out.println("\n");
         while(option>=0 && option<=2){
             option=FinanceDevelopment.menufinance(file1);
-            switch (option){
-                case 1: 
-                    FinanceDevelopment.addAmount(file1);
-                    break;
-                case 2: 
-                    FinanceDevelopment.subtractAmount(file1);
+            if(option==1){
+                FinanceDevelopment.addAmount(file1);
+            }
+            if(option==2){
+                FinanceDevelopment.subtractAmount(file1);
             }
         }
     } 
