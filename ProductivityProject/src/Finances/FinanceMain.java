9c90ea3ae16dde5d6d646e43a56a0373a75970bc
Jose <joseamendezborges@gmail.com>
2025-02-12 23:02:53 -0400

    Changed file names

--- ProductivityProject/src/Finances/FinanceMain.java
+++ ProductivityProject/src/Finances/FinanceMain.java
@@ -4,8 +4,8 @@ import java.io.IOException;
 public class FinanceMain {
     public static void main(String username) throws IOException{
         double option=0;
-        String file1 = username+"finances1.txt";
-        String file2 = username+"finances2.txt";
+        String file1 = "Finances."+username+".txt";
+        String file2 = "History."+username+".txt";
         FinanceDevelopment.funDefault(file1, file2);
         System.out.println("\n");
         while(option>=0 && option<=2){
