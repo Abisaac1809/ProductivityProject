0b61e853fbf42f0b55725ddc80aa25aebc6203fe
Gabriel Vieira <nox030705@gmail.com>
2025-02-13 09:14:30 -0400

    added proper validations in UsersValidation

--- ProductivityProject/src/Users/UsersValidation.java
+++ ProductivityProject/src/Users/UsersValidation.java
@@ -30,88 +30,100 @@ public class UsersValidation {
     }    
     private static boolean checkChars(String value) {
         String unsupported = "1234567890.,?/:;'!@#$%^&*()[]{}\\|=-+_~";
-        for (int i = 0; i < value.length(); i++) {
-            for (int j = 0; j < unsupported.length(); j++) {
-                if (unsupported.charAt(j) == value.charAt(i)) {
-                    return false;
+        if (value != null) {
+            for (int i = 0; i < value.length(); i++) {
+                for (int j = 0; j < unsupported.length(); j++) {
+                    if (unsupported.charAt(j) == value.charAt(i)) {
+                        return false;
+                    }
+                }
+            }
+            return true;
+        }
+        return false;
+    }
+    static String username(boolean isLogin, Scanner input) {
+        String value = "";
+        if (input != null) {
+            while (true) {
+                value = input.next();
+                if (userExists(value) && !isLogin) {
+                    System.out.println("Error: [Nombre de usuario ya en uso]");
+                    System.out.print("- Ingrese otro nombre de usuario: ");
+                } else if (!userExists(value) && isLogin) {
+                    System.out.println("Error: [Nombre de usuario no existe]");
+                    System.out.print("- Ingrese otro nombre de usuario: ");
+                } else if (checkChars(value)) {
+                    return value;
+                } else {
+                    System.out.println("Error: [Caracter invalido encontrado]");
+                    System.out.print("- Ingrese un nombre correcto: ");
                 }
             }
         }
-        return true;
-    }
-    static String username(boolean isLogin, Scanner input) {
-        String value;
-        while (true) {
-            value = input.next();
-            if (userExists(value) && !isLogin) {
-                System.out.println("Error: [Nombre de usuario ya en uso]");
-                System.out.print("- Ingrese otro nombre de usuario: ");
-            } else if (!userExists(value) && isLogin) {
-                System.out.println("Error: [Nombre de usuario no existe]");
-                System.out.print("- Ingrese otro nombre de usuario: ");
-            } else if (checkChars(value)) {
-                return value;
-            } else {
-                System.out.println("Error: [Caracter invalido encontrado]");
-                System.out.print("- Ingrese un nombre correcto: ");
-            }
-        }
+        return value;
     }
     static void saveUser(String username, String password) {
-        try {
-            String path = Paths.get(".").toRealPath().toString() + "/src/Users/users.txt";
-            File file = new File(path);
-            file.createNewFile();
-            FileWriter writer = new FileWriter(path, true);
-            writer.write(String.format("%-20s  %-20s\n", username, password));
-            writer.close();
-            System.out.println("Usuario Creado!");
-        } catch (IOException e) {
-            System.out.println("Error: " + e.getMessage());
+        if (username != null && password != null) {
+            try {
+                String path = Paths.get(".").toRealPath().toString() + "/src/Users/users.txt";
+                File file = new File(path);
+                file.createNewFile();
+                FileWriter writer = new FileWriter(path, true);
+                writer.write(String.format("%-20s  %-20s\n", username, password));
+                writer.close();
+                System.out.println("Usuario Creado!");
+            } catch (IOException e) {
+                System.out.println("Error: " + e.getMessage());
+            }
         }
     }
     private static boolean userExists(String username) {
-        try {
-            String path = Paths.get("").toRealPath().toString() + "/src/Users/users.txt";
-            File file = new File(path);
-            file.createNewFile();
-            Scanner fileScanner = new Scanner(file);
-            while (fileScanner.hasNextLine()) {
-                if (fileScanner.next().equals(username)) {
-                    fileScanner.close();
-                    return true;
+        if (username != null) {
+            try {
+                String path = Paths.get("").toRealPath().toString() + "/src/Users/users.txt";
+                File file = new File(path);
+                file.createNewFile();
+                Scanner fileScanner = new Scanner(file);
+                while (fileScanner.hasNextLine()) {
+                    if (fileScanner.next().equals(username)) {
+                        fileScanner.close();
+                        return true;
+                    }
+                    fileScanner.nextLine();
                 }
-                fileScanner.nextLine();
+                fileScanner.close();
+            } catch (IOException e) {
+                System.out.println("Error: " + e.getMessage());
             }
-            fileScanner.close();
-        } catch (IOException e) {
-            System.out.println("Error: " + e.getMessage());
         }
         return false;
     }
     static void checkPassword(String username, Scanner input) {
-        while (true) {
-            try {
-                String path = Paths.get("").toRealPath().toString() + "/src/Users/users.txt";
-                Scanner file = new Scanner(new File(path));
-                String p = "";
-                System.out.print("- Contraseña: ");
-                String password = encrypt(input.next());
-                while (file.hasNextLine()) {
-                    if (file.next().equals(username)) {
-                        p = file.next();
+        if (username != null && input != null) {
+            while (true) {
+                try {
+                    String path = Paths.get("").toRealPath().toString() + "/src/Users/users.txt";
+                    Scanner file = new Scanner(new File(path));
+                    String p = "";
+                    System.out.print("- Contraseña: ");
+                    String password = encrypt(input.next());
+                    while (file.hasNextLine()) {
+                        if (file.next().equals(username)) {
+                            p = file.next();
+                        }
+                        file.nextLine();
                     }
-                    file.nextLine();
-                }
-                if (p.equals(password)) {
+                    if (p.equals(password)) {
+                        file.close();
+                        return;
+                    } else {
+                        System.out.println("Error: [Contraseña inválida]");
+                    }
                     file.close();
-                    return;
-                } else {
-                    System.out.println("Error: [Contraseña inválida]");
+                } catch (IOException e) {
+                    System.out.println("Error: " + e.getMessage());
                 }
-                file.close();
-            } catch (IOException e) {
-                System.out.println("Error: " + e.getMessage());
             }
         }
     }
@@ -164,27 +176,31 @@ public class UsersValidation {
         return encryp;
     }
     static void createFinancesFile(String username) {
-        try {
-            String path1 = Paths.get(".").toRealPath().toString() + "/src/Finances/" + username + "finances1.txt";
-            String path2 = Paths.get(".").toRealPath().toString() + "/src/Finances/" + username + "finances2.txt";
-            File file1 = new File(path1);
-            file1.createNewFile();
-            File file2 = new File(path2);
-            file2.createNewFile();
-        } catch (IOException e) {
-            System.out.println("Error: " + e.getMessage());
+        if (username != null) {
+            try {
+                String path1 = Paths.get(".").toRealPath().toString() + "/src/Finances/" + username + "finances1.txt";
+                String path2 = Paths.get(".").toRealPath().toString() + "/src/Finances/" + username + "finances2.txt";
+                File file1 = new File(path1);
+                file1.createNewFile();
+                File file2 = new File(path2);
+                file2.createNewFile();
+            } catch (IOException e) {
+                System.out.println("Error: " + e.getMessage());
+            }
         }
     }
     static void createHabitsFile(String username) {
-        try {
-            String path1 = Paths.get(".").toRealPath().toString() + "/src/Habits/HabitsDataBase/Habits." + username + ".txt";
-            String path2 = Paths.get(".").toRealPath().toString() + "/src/Habits/HabitsDataBase/Performance." + username + ".txt";
-            File file1 = new File(path1);
-            file1.createNewFile();
-            File file2 = new File(path2);
-            file2.createNewFile();
-        } catch (IOException e) {
-            System.out.println("Error: " + e.getMessage());
+        if (username != null) {
+            try {
+                String path1 = Paths.get(".").toRealPath().toString() + "/src/Habits/HabitsDataBase/Habits." + username + ".txt";
+                String path2 = Paths.get(".").toRealPath().toString() + "/src/Habits/HabitsDataBase/Performance." + username + ".txt";
+                File file1 = new File(path1);
+                file1.createNewFile();
+                File file2 = new File(path2);
+                file2.createNewFile();
+            } catch (IOException e) {
+                System.out.println("Error: " + e.getMessage());
+            }
         }
     }
 }
