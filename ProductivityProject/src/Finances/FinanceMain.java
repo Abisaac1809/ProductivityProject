a55999aca8f792fb613b15fdee69e4a4377c365e
Jose <joseamendezborges@gmail.com>
2025-02-12 21:41:24 -0400

    Added camelcase

--- ProductivityProject/src/Finances/FinanceMain.java
+++ ProductivityProject/src/Finances/FinanceMain.java
@@ -6,10 +6,10 @@ public class FinanceMain {
         double option=0;
         String file1 = username+"finances1.txt";
         String file2 = username+"finances2.txt";
-        FinanceDevelopment.fundefault(file1, file2);
+        FinanceDevelopment.funDefault(file1, file2);
         System.out.println("\n");
         while(option>=0 && option<=2){
-            option=FinanceDevelopment.menufinance(file1);
+            option=FinanceDevelopment.menuFinance(file1);
             if(option==1){
                 FinanceDevelopment.addAmount(file1);
             }
