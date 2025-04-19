b3981f9195aceef04fc5410dae328cb6822a01aa
Gabriel Vieira <nox030705@gmail.com>
2025-04-19 00:52:47 -0400

    added validations for `Task` TDA

--- ProductivityProject/src/Repositories/Task.java
+++ ProductivityProject/src/Repositories/Task.java
@@ -6,17 +6,45 @@ public class Task {
 	private String status;
 
 	public Task(String title, String description, String status) {
+		if (title == null)
+			throw new MissingArgumentException("title");
+		if (description == null)
+			throw new MissingArgumentException("description");
+		if (status == null)
+			throw new MissingArgumentException("status");
+		validateTitle(title);
+		validateDescription(description);
+		validateStatus(status);
 		this.title = title;
 		this.description = description;
 		this.status = status;
 	}
+
 	public String getTitle() {
 		return this.title;
 	}
+
 	public String getStatus() {
 		return this.status;
 	}
+
 	public String getDescription() {
 		return this.description;
+	}
+
+	private void validateTitle(String title) {
+		boolean isValid = title.length() > 0 && title.length() <= 20;
+		if (!isValid)
+			throw new ObjectValidationException("titulo debe contener entre 1 y 20 caracters");
+	}
+
+	private void validateDescription(String description) {
+		if (description.isEmpty())
+			throw new ObjectValidationException("la descripcion debe tener minimo 1 caracter");
+	}
+
+	private void validateStatus(String status) {
+		if (status != "No Hecha" && status != "Hecha")
+			throw new ObjectValidationException("estado de tarea incorrecto [" + status + "]");
 	}
 }
